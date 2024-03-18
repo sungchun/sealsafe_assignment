a4edb13cbb10767fc3f2a0cc6dd50ecaf95f9611
Preston <preston.ng37@gmail.com>
2024-03-18 23:23:28 +0800

    search bar successfully makes get request

--- api/api/settings.py
+++ api/api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'api.urls'
@@ -122,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
--- src/components/Search.js
+++ src/components/Search.js
@@ -4,27 +4,33 @@ import axios from "axios"
 function Search() {
     const [input, setInput] = useState('')
 
-    function handleChange(event) {
-        setInput(event.target.value)
-    }
-    
-    async function handleSubmit(event) {
-        event.preventDefault()
-        event.target.value = ''
-        const config = {
-            method: "get",
-            url: `api/city-weather/${input}`,
+        function handleChange(event) {
+            setInput(event.target.value)
         }
-        
-        const response = await axios(config)
-        return response.data
-    }
+    
+        async function handleSubmit(event) {
+            event.preventDefault()
+            const config = {
+                method: "get",
+                url: `http://127.0.0.1:8000/api/city-weather/${input}/`,
+                header: {
+                    'Content-Type': 'application/json'
+                },
+            }
+            try {
+                const response = await axios(config)
+                console.log(response)
+                return response.data
+            } catch (error) {
+                console.log(error)
+            }
+        }
     
     return (
         <form onChange={handleChange} onSubmit={handleSubmit}>
             <input type="text" placeholder="Type City Name..."/>
         </form>
-    );
+    )
 }
 
 export default Search;
